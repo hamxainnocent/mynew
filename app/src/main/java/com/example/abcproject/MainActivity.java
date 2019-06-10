fbcc129ca2dee911dad22886b858a29089cb8c0a
Muhammad Hamza <muhammadhamza711177@gmail.com>
2019-06-10 12:39:26 +0500

    commit1

--- app/src/main/java/com/example/abcproject/MainActivity.java
+++ app/src/main/java/com/example/abcproject/MainActivity.java
@@ -10,4 +10,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void testapp(){
+        int a = 1+1;
+    }
 }
